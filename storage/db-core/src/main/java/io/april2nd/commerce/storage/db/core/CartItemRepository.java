1187edff5c69280eba7945b47b5175085de98059
7unho <rlawnsgh8395@naver.com>
2026-06-10 14:08:32 +0900

    Feat: 공유 장바구니 저장 모델 추가

--- storage/db-core/src/main/java/io/april2nd/commerce/storage/db/core/CartItemRepository.java
+++ storage/db-core/src/main/java/io/april2nd/commerce/storage/db/core/CartItemRepository.java
@@ -7,9 +7,11 @@ import java.util.List;
 import java.util.Optional;
 
 public interface CartItemRepository extends JpaRepository<CartItemEntity, Long> {
-    List<CartItemEntity> findByUserIdAndStatus(Long userId, EntityStatus status);
+    List<CartItemEntity> findByCartIdAndStatus(Long cartId, EntityStatus status);
 
-    CartItemEntity findByUserIdAndProductId(Long userId, Long productId);
+    List<CartItemEntity> findByCartIdInAndStatus(List<Long> cartIds, EntityStatus status);
 
-    Optional<CartItemEntity> findByUserIdAndIdAndStatus(Long userId, Long id, EntityStatus status);
+    CartItemEntity findByCartIdAndProductId(Long cartId, Long productId);
+
+    Optional<CartItemEntity> findByIdAndStatus(Long id, EntityStatus status);
 }
